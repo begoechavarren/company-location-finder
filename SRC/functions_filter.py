c078a6e565657d8ac3d334dcdc68e4742b92711b
Bego Echavarren <begona.echavarren@gmail.com>
2019-09-18 23:50:53 +0200

    structure

--- SRC/functions_filter.py
+++ SRC/functions_filter.py
@@ -3,10 +3,12 @@ import pandas as pd
 from pymongo import MongoClient
 import time
 import requests
+from dotenv import load_dotenv
+import os
+from sklearn import preprocessing
+
 
 # functions filter_categories
-
-
 def categories_builder(txt):
     regex = ""
     categories = [cat.strip() for cat in txt.split(",")]
@@ -41,6 +43,20 @@ def df_creator(lst_coord):
     df = pd.DataFrame(df_list, columns=["coords", "near_companies"])
     return df
 
+
+def filter_categories(companies, coord_list, min_money_raised, cat_regex):
+    # get the cleaned database from mongo
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.companies
+    # filter by category and money raised
+    # !!!!!!!!!!!cat_regex y min_money_raised deberían venir de los argumentos!!!! (por ahora los defino)
+    selected = db.selected.find({"$and": [{"total_money_raised": {"$gte": min_money_raised}},
+                                          {"$or": [{"tag_list": {"$regex": cat_regex}}, {"category_code": {"$regex": cat_regex}}, {"name": {"$regex": cat_regex}}, {"description": {"$regex": cat_regex}}, {"overview": {"$regex": cat_regex}}]}]})
+    selected_df = pd.DataFrame(selected)
+    # create dataframe based on coords which have more companies of the selected category next to them
+    my_df = df_creator(coord_list)
+    return my_df
+
 # functions filter_maps
 # https://developers.google.com/places/web-service/get-api-key
 # https://console.cloud.google.com/billing/0163C0-E6306C-B76C83?project=project-ironhack-pymongo
@@ -64,8 +80,20 @@ def countGoogledata(place, radius, coord, key):
 
 def create_hostelry(place, radius, coord, key):
     count = countGoogledata(place, radius, coord, key)
-    print(count)
     return count
+
+
+def filter_maps(df):
+    load_dotenv()
+    google_key = os.getenv("google_key")
+    # importaba my_df
+    # ojo esto hacerlo con parámetros!
+    # y ojo palabras que meta el usuario que sean separadas con comas
+    df['hostelry'] = df.apply(lambda x: create_hostelry(
+        "starbucks", 1500, x["coords"], google_key), axis=1)
+    df['services'] = df.apply(lambda x: create_hostelry(
+        "elementary school", 1500, x["coords"], google_key), axis=1)
+    return df
 
 
 # functions filter_meetup
@@ -74,5 +102,36 @@ def countMeetupdata(coord, category, key):
         str(coord[0]), category, str(coord[1]), key)
     data = (requests.get(url)).json()
     count = len([x['id'] for x in data['events']])
-    print(count)
     return count
+
+
+def filter_meetup(category, df):
+    load_dotenv()
+    meetup_key = os.getenv("meetup_key")
+    df['events'] = df.apply(lambda x: countMeetupdata(
+        x["coords"], category, meetup_key), axis=1)
+    return df
+
+
+# functions punctuation
+
+def normalize_df(df):
+    df.set_index('coords', inplace=True)
+    x = df.values  # returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    new_df = pd.DataFrame(x_scaled)
+    new_df.columns = ["near_companies", "hostelry", "services", "events"]
+    df['near_companies'] = list(df['near_companies'])
+    df['hostelry'] = list(df['hostelry'])
+    df['services'] = list(df['services'])
+    df['events'] = list(df['events'])
+    return df
+
+
+def punctuation(df, b_near_companies, b_hostelry, b_services, b_events):
+    df['punctuation'] = df['near_companies']*b_near_companies + \
+        df['hostelry']*b_hostelry+df['services'] * \
+        b_services+df['events']*b_events
+    final_location = df[df['punctuation'] == max(df['punctuation'])].index[0]
+    return final_location
--- SRC/main.py
+++ SRC/main.py
@@ -1,7 +1,30 @@
+from clean import companies, coord_list
+from functions_filter import *
+
+
+cat_regex = "(?i)tech|(?i)web|(?i)design|(?i)code|(?i)mobile|(?i)advertising"
+min_money_raised = 1000000
+category = "562"
+b_near_companies = 1
+b_hostelry = 2
+b_services = 4
+b_events = 3
+
+# en main tengo que meter como parametros los argumentos no?
+
 
 def main():
-    pass
+    my_df = filter_categories(companies, coord_list,
+                              min_money_raised, cat_regex)
+    my_df = filter_maps(my_df)
+    my_df = filter_meetup(category, my_df)
+    my_df = normalize_df(my_df)
+    final_location = punctuation(
+        my_df, b_near_companies, b_hostelry, b_services, b_events)
+    return final_location
 
+
+print(main())
 
 if __name__ == '__main__':
     main()
